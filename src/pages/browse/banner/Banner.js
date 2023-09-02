ef1fcf9a02e0f8c435955cd3bece2089da21989f
Dao Xuan Dung <dungdxfx21334@funix.edu.vn>
2023-09-02 23:43:23 +0700

    Trivial change, probably code style

--- src/pages/browse/banner/Banner.js
+++ src/pages/browse/banner/Banner.js
@@ -2,15 +2,54 @@ import useFetchMovies from '../../../hooks/useFetchMovies'
 import { ApiKeyContext } from '../../../context/api-context'
 import { useContext } from 'react'
 import React from 'react'
+import Button from '../../../UI/Button'
+import classes from './Banner.module.css'
 
 const Banner = () => {
   const API_KEY = useContext(ApiKeyContext).API_KEY
   const url = `https://api.themoviedb.org/3//discover/tv?api_key=${API_KEY}&with_network=123`
 
-  const moviesList = useFetchMovies(url)
-  console.log(moviesList)
-  return <></>
+  const { moviesList, isLoading } = useFetchMovies(url)
+
+  if (isLoading) {
+    // Making sure no code is executed before the movies are successfully retrieved
+    return <p>Loading...</p>
+  }
+
+  const bannerSelectedMovie =
+    moviesList.results[
+      Math.floor(Math.random() * moviesList.results.length - 1)
+    ]
+
+  const backdropPath = `https://image.tmdb.org/t/p/original${bannerSelectedMovie['backdrop_path']}`
+
+  // Make overview text shorter
+  const overviewMaxCharacters =
+    bannerSelectedMovie.overview.length < 100
+      ? bannerSelectedMovie.overview.length
+      : 100
+  const contractedOverview =
+    bannerSelectedMovie.overview.substr(0, overviewMaxCharacters) + '...'
+
+  return (
+    <div
+      style={{ backgroundImage: `url(${backdropPath})` }}
+      className={classes.container}
+    >
+      <div className={classes.bannerTexts}>
+        <h1>{bannerSelectedMovie.name}</h1>
+        <div className={classes.buttonGroup}>
+          <Button type='button' className={classes.bannerBtn}>
+            Play
+          </Button>
+          <Button type='button' className={classes.bannerBtn}>
+            My List
+          </Button>
+        </div>
+        <p className={classes.overview}>{contractedOverview}</p>
+      </div>
+    </div>
+  )
 }
 
 export default Banner
-// `https://api.themoviedb.org/3/trending/all/week?api_key=${API_KEY}&language=en-US`
