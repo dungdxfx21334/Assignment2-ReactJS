43901cd6c459a0595a2aee0907f11212f804d1a1
Dao Xuan Dung <dungdxfx21334@funix.edu.vn>
2023-09-03 13:02:20 +0700

    Moving search icon into a new file

--- src/pages/search/Search.jsx
+++ src/pages/search/Search.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import Navbar from '../browse/navbar/Navbar';
 
 const Search = () => {
   return (
     <div className="app">
-      <h1>Search</h1>
+      <Navbar></Navbar>
     </div>
   );
 };
