fcf1e34c09a2528cfb75c9de4c59c9e8a606af1b
Dao Xuan Dung <dungdxfx21334@funix.edu.vn>
2023-09-03 15:48:21 +0700

    Adding SearchForm component

--- src/pages/search/SearchForm/SearchForm.js
+++ src/pages/search/SearchForm/SearchForm.js
@@ -1,27 +1,59 @@
-import React from 'react'
+import React, { Fragment } from 'react'
 import SearchIcon from '../SearchIcon'
 
 import Button from '../../../UI/Button'
 import classes from './SearchForm.module.css'
-const SearchForm = () => {
-  return (
-    <form className={classes.form}>
-      <div className={classes.searchInput}>
-        <input type='text'></input>
-        <span className={classes.icon}>
-          <SearchIcon></SearchIcon>
-        </span>
-      </div>
+import { useRef } from 'react'
+import { useContext } from 'react'
+import { ApiKeyContext } from '../../../context/api-context'
+import { useState } from 'react'
 
-      <div className={classes.btnGroup}>
-        <Button type='button' className={classes.btnCancel}>
-          Cancel
-        </Button>
-        <Button type='submit' className={classes.btnSearch}>
-          Search
-        </Button>
-      </div>
-    </form>
+import ResultList from '../result/ResultList'
+const SearchForm = () => {
+  const inputSearchRef = useRef()
+  const API_KEY = useContext(ApiKeyContext).API_KEY
+  const [showResult, setShowResult] = useState(false)
+  const [resultList, setResultList] = useState([])
+
+  const searchHandler = async event => {
+    event.preventDefault()
+    try {
+      console.log(inputSearchRef)
+      const url = `https://api.themoviedb.org/3/search/movie?query=${inputSearchRef.current.value}&api_key=${API_KEY}&language=en-US`
+      const response = await fetch(url)
+      if (!response.ok) {
+        throw new Error('No moives found')
+      }
+      const data = await response.json()
+      console.log(data)
+      setResultList(data)
+      setShowResult(true)
+    } catch (error) {
+      console.log(error.message)
+    }
+  }
+  return (
+    <Fragment>
+      <form className={classes.form} onSubmit={searchHandler}>
+        <div className={classes.searchInput}>
+          <input type='text' ref={inputSearchRef}></input>
+          <span className={classes.icon}>
+            <SearchIcon></SearchIcon>
+          </span>
+        </div>
+
+        <div className={classes.btnGroup}>
+          <Button type='button' className={classes.btnCancel}>
+            Cancel
+          </Button>
+          <Button type='submit' className={classes.btnSearch}>
+            Search
+          </Button>
+        </div>
+      </form>
+
+      {showResult && <ResultList resultList={resultList}></ResultList>}
+    </Fragment>
   )
 }
 
